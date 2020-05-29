b593cd79509d42ae92a54fa3ddc1da82ea161778
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-05-30 00:14:36 +0200

    Initialize bot application

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,43 @@
-console.log('It works!');
-console.log('Watching works!');
+import { App } from '@slack/bolt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+
+app.message('hello', async ({ message, say }) => {
+  await say({
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `Hey there <@${message.user}>!`
+        },
+        accessory: {
+          type: "button",
+          text: {
+            type: "plain_text",
+            text: "Click Me"
+          },
+          action_id: "button_click"
+        }
+      }
+    ],
+    text: null,
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  await ack();
+
+  await say(`<@${body.user.id}> clicked the button`);
+});
+
+app.start(process.env.PORT || 3000)
+  .catch(console.error)
+  .then(() => console.log('⚡️ Bolt app is running!'))
