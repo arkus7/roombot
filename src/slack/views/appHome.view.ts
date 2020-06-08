6acf04ab4875ec59e24061207d858db383175411
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-06-08 22:08:16 +0200

    Fix lint erros in appHome view

--- src/slack/views/appHome.view.ts
+++ src/slack/views/appHome.view.ts
@@ -7,14 +7,12 @@ export function appHomeView(): View {
   return {
     type: 'home',
     callback_id: 'home_view',
-
-    /* body of the view */
     blocks: [
       {
         type: 'section',
         text: {
-          'type': 'mrkdwn',
-          'text': '*Welcome to your _App\'s Home_* :tada:'
+          type: 'mrkdwn',
+          text: '*Welcome to your _App\'s Home_* :tada:'
         }
       },
       DIVIDER_BLOCK,
