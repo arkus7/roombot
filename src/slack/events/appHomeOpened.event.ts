41088d5d7450a552968d0f792ce6448dac9ddc54
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2020-09-12 22:27:36 +0200

    Fix const export

--- src/slack/events/appHomeOpened.event.ts
+++ src/slack/events/appHomeOpened.event.ts
@@ -1,20 +1,20 @@
 import { EventHandler } from '../utils/types';
 import { appHomeView } from '../views/appHome.view';
 
-export const APP_HOME_OPENED_EVENT = 'app_home_opened';
+export const APP_HOME_OPENED_EVENT = 'app_home_opened' as const;
 
 export function appHomeOpenedHandler(): EventHandler<typeof APP_HOME_OPENED_EVENT> {
   return async ({ event, context, client }) => {
     try {
       /* view.publish is the method that your app uses to push a view to the Home tab */
       await client.views.publish({
-  
+
         /* retrieves your xoxb token from context */
         token: context.botToken,
-  
+
         /* the user that opened your app's app home */
         user_id: event.user,
-  
+
         /* the view payload that appears in the app home*/
         view: appHomeView()
       });
